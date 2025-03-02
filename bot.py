34c433bcab94839a4c4a63f898eb6c1677d97817
moha45678 <147262982+moha45678@users.noreply.github.com>
2025-03-02 15:29:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @mr17
+# Subscribe YouTube Channel For Amazing Bot @mr18
+# Ask Doubt on telegram @mr19
 
-# Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
+# Clone Code Credit : YT - @mr20 / TG - @mr21 / GitHub - @mr22
 
 import sys, glob, importlib, logging, logging.config, pytz, asyncio
 from pathlib import Path
@@ -29,20 +29,20 @@ from datetime import date, datetime
 from aiohttp import web
 from plugins import web_server
 
-from TechVJ.bot import TechVJBot
-from TechVJ.util.keepalive import ping_server
-from TechVJ.bot.clients import initialize_clients
+from mr23.bot import mr24Bot
+from mr25.util.keepalive import ping_server
+from mr26.bot.clients import initialize_clients
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
-TechVJBot.start()
+mr27Bot.start()
 loop = asyncio.get_event_loop()
 
 
 async def start():
     print('\n')
     print('Initalizing Your Bot')
-    bot_info = await TechVJBot.get_me()
+    bot_info = await mr28Bot.get_me()
     await initialize_clients()
     for name in files:
         with open(name) as a:
@@ -54,19 +54,19 @@ async def start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Tech VJ Imported => " + plugin_name)
+            print("mr29 Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
-    me = await TechVJBot.get_me()
-    temp.BOT = TechVJBot
+    me = await mr30.get_me()
+    temp.BOT = mr31Bot
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
-    tz = pytz.timezone('Asia/Kolkata')
+    tz = pytz.timezone('mr32')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await TechVJBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await mr33Bot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
